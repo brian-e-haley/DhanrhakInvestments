bf2b4abcd60e8430c6cf5ecbbd5239682d72e168
Brian Haley <brian_haley@live.ca>
2020-09-29 22:17:10 -0400

    Add users to either student or teacher role
    Signed-off-by: Brian Haley <brian_haley@live.ca>

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,21 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User
 
 
 class UserRegisterForm(UserCreationForm):
+    ROLES = (('student', 'Student'), ('teacher', 'Teacher'))
+    role = forms.ChoiceField(choices=ROLES)
+
+    def save(self, commit=True):
+        user = super().save()
+        student = Group.objects.get(name='student')
+        teacher = Group.objects.get(name='teacher')
+        if self.cleaned_data['role'] == 'student':
+            student.user_set.add(user)
+        else:
+            teacher.user_set.add(user)
+
     class Meta:
         model = User
         fields = [
