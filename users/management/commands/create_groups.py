06106df7367eca24a60fca43f8b11bc0b769e1ff
Brian Haley <brian_haley@live.ca>
2020-09-29 20:14:23 -0400

    Command to create student and teacher groups
    Signed-off-by: Brian Haley <brian_haley@live.ca>

---
+++ users/management/commands/create_groups.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Group
+from django.core.management.base import BaseCommand
+
+GROUPS = {'student', 'teacher'}
+APPS = {'quizzes'}
+PERMISSIONS = {'add', 'change', 'delete', 'view'}
+MODELS = {'Quiz', 'Question', 'Option'}
+
+
+class Command(BaseCommand):
+    help = 'Creates teacher and student groups.'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Creating the student and teacher group.')
+        Group.objects.get_or_create(name='student')
+        Group.objects.get_or_create(name='teacher')
+        self.stdout.write('Student and teacher groups created successfully.')
