a4ed254f3696567dcb34e5d35b27f100c9332f31
Brian Haley <brian_haley@live.ca>
2020-09-30 17:33:20 -0400

    Quiz detail view
    Signed-off-by: Brian Haley <brian_haley@live.ca>

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('quiz/new/',
          profile_views.QuizCreateView.as_view(),
          name='quiz_create'),
+    path('quiz/<int:pk>/',
+         profile_views.QuizDetailView.as_view(),
+         name='quiz_detail'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
 from profiles.models import Quiz
 
@@ -8,7 +8,6 @@ class ProfileView(LoginRequiredMixin, ListView):
     template_name = 'profiles/profile.html'
     model = Quiz
     context_object_name = 'quizzes'
-    ordering = ['-pk']
 
     def get_queryset(self):
         return Quiz.objects.filter(teacher=self.request.user).all()
@@ -17,7 +16,12 @@ class ProfileView(LoginRequiredMixin, ListView):
 class QuizCreateView(LoginRequiredMixin, CreateView):
     model = Quiz
     fields = ['title', 'description']
+    context_object_name = 'quiz'
 
     def form_valid(self, form):
         form.instance.teacher = self.request.user
         return super().form_valid(form)
+
+
+class QuizDetailView(LoginRequiredMixin, DetailView):
+    model = Quiz
